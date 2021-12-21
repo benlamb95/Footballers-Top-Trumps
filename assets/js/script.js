be6abc1578e8937464b6754c57ca980c81cb749c
benlamb95 <benl1995@hotmail.co.uk>
2021-12-21 18:10:00 +0000

    Add intial card value to script file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,45 @@
-console.log("connected");
+let footballers = [
+    {
+        image: 'c-ronaldo.jpg',
+        name: 'Cristiano Ronaldo',
+        Attacking: '98',
+        Midfield: '85',
+        Defence: '10',
+        Goalkeeping: '0',
+    },
+    {
+        name: 'Lionel Messi',
+        Attacking: '99',
+        Midfield: '84',
+        Defence: '10',
+        Goalkeeping: '0',
+    },
+    {
+        name: 'Neymar',
+        Attacking: '96',
+        Midfield: '80',
+        Defence: '0',
+        Goalkeeping: '0',
+    },
+    {
+        name: 'Kylian Mbappe',
+        Attacking: '95',
+        Midfield: '74',
+        Defence: '10',
+        Goalkeeping: '0',
+    },
+    {
+        name: 'Ngolo Kante',
+        Attacking: '50',
+        Midfield: '97',
+        Defence: '80',
+        Goalkeeping: '0',
+    },
+    {
+        name: 'Kevin De Bruyne',
+        Attacking: '98',
+        Midfield: '85',
+        Defence: '10',
+        Goalkeeping: '0',
+    }
+]
