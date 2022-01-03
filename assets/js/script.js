7e706f164af627493358d3b790e71389953f3c76
benlamb95 <benl1995@hotmail.co.uk>
2022-01-03 15:36:32 +0000

    Add Modal script with help from w3 schools

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,21 @@
+// Modal Script
 
+var modal = document.getElementById("playModal")
 
+var button = document.getElementById("how-to-play")
+
+var span = document.getElementsByClassName("close")[0]
+
+button.onclick = function () {
+    modal.style.display = "block"
+}
+
+span.onclick = function () {
+    modal.style.display = "none"
+}
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
