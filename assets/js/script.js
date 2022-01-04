17811144c1061545c4765975e3f5f1261979bc9c
benlamb95 <benl1995@hotmail.co.uk>
2022-01-04 12:38:26 +0000

    Play audio template added but not working- will attempt to fix another time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,31 @@
-// Modal Script
+/* -- Audio variables -- */
+
+var backgroundAudio = new Audio('/assets/audio/crowd.wav');
+var playPauseIcon = document.getElementById('play-pause');
+backgroundAudio.volume = 0.3;
+backgroundAudio.loop = true;
+
+
+
+
+/* -- Play/Pause Music Script -- */
+// Video from https://www.youtube.com/watch?v=wffK2OIt8u0 
+
+function playPause() {
+    if (count == 0) {
+        count = 1;
+        backgroundAudio.play();
+        playPauseIcon.className = "fas fa-volume-up";
+    } else {
+        count = 0;
+        backgroundAudio.pause();
+        playPauseIcon.className = "fas fa-volume-mute";
+    }
+};
+
+
+
+/* -- Modal Script -- */
 
 var modal = document.getElementById("playModal")
 
